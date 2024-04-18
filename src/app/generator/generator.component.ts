558b6efdcabca5060565bd42c7b0701e1eab47ec
amiloszewicz <miloszewicz.adam@gmail.com>
2024-04-18 15:13:40 +0200

    feat component: generator

---
+++ src/app/generator/generator.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generator',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      generator works!
+    </p>
+  `,
+  styles: ``
+})
+export class GeneratorComponent {
+
+}
