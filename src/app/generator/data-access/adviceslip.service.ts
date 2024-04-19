70a79ad32819f394d8c0e4c7ac70d0c861033a60
amiloszewicz <miloszewicz.adam@gmail.com>
2024-04-19 13:47:36 +0200

    refactor service: add state for advice

--- src/app/generator/data-access/adviceslip.service.ts
+++ src/app/generator/data-access/adviceslip.service.ts
@@ -1,17 +1,53 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { catchError, distinctUntilChanged, EMPTY, map } from 'rxjs';
 import {
   Advice,
   AdviceSlipResponse,
 } from '../../shared/data-access/interfaces/advice';
 
+export interface AdviceState {
+  advice: Advice;
+  error: string | null;
+  loading: boolean;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class AdviceslipService {
   http = inject(HttpClient);
-  constructor() {}
+
+  // state
+  state = signal<AdviceState>({
+    advice: {
+      name: null,
+      number: null,
+    },
+    error: null,
+    loading: true,
+  });
+
+  // selectors
+  advice = computed(() => this.state().advice);
+  loading = computed(() => this.state().loading);
+
+  // sources
+  loadAdvice$ = this.fetchFromAdviceSlip();
+
+  constructor() {
+    // reducers
+    this.loadAdvice$.pipe(takeUntilDestroyed()).subscribe({
+      next: (advice) =>
+        this.state.update((state) => ({
+          ...state,
+          advice: advice,
+          loading: false,
+        })),
+      error: (err) => this.state.update((state) => ({ ...state, error: err })),
+    });
+  }
 
   fetchFromAdviceSlip() {
     return this.http
